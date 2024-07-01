a3c7b0356b6af54d0b50f0e206e2f56a15ad20c2
Bhushan Shrirame <bhushanshrirame24@gmail.com>
2024-07-01 11:55:25 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,39 @@
+export const sidebarLinks = [
+    {
+        lable:'Home',
+        imgUrl: '/icons/Home.svg',
+        route: '/',
+
+    },
+    {
+        lable:'Upcoming',
+        imgUrl: '/icons/upcoming.svg',
+        route: '/upcoming',
+
+    },
+    {
+        lable:'Previous',
+        imgUrl: '/icons/previous.svg',
+        route: '/previous',
+
+    },
+    {
+        lable:'Recordings',
+        imgUrl: '/icons/Video.svg',
+        route: '/recordings',
+
+    },
+    {
+        lable:'Personal Room',
+        imgUrl: '/icons/add-personal.svg',
+        route: '/personal-room',
+
+    },
+]
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+  ];
